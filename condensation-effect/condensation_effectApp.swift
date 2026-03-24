06f7a75b8006382ea5e3605da9551de152cf394a
e <e>
2026-03-24 12:14:11 +0300

    Initial Commit

---
+++ condensation-effect/condensation_effectApp.swift
@@ -0,0 +1,17 @@
+//
+//  condensation_effectApp.swift
+//  condensation-effect
+//
+//  Created by Pavel Korostelev on 24.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct condensation_effectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
